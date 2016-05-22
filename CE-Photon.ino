e82ae2e6da6cb8fbdbe734679fe8ce4a9eba76f5
Kabelaffe <kabel42@gmail.com>
2016-05-22 08:28:59 -0700

    Check sonnected Sensors

--- CE-Photon.ino
+++ CE-Photon.ino
@@ -1,7 +1,9 @@
 #include <math.h>
+#include <list>
 
 #include "Sensor.h"
 #include "HTU20D.h"
+#include "ADC121C.h"
 
 Sensor *S1; //I2C 1
 Sensor *S2; //I2C 2
@@ -9,6 +11,7 @@ Sensor *S3; //I2C 3
 Sensor *S4; //I2C 4
 
 char data[255];
+int delaytime = 60000;
 
 void setup()
 {
@@ -16,16 +19,29 @@ void setup()
   Wire.setSpeed(CLOCK_SPEED_100KHZ);
   Wire.begin();
 
+  for(uint8_t i=0; i<128; i++)
+  {
+    Wire.beginTransmission(i);
+    int error = Wire.endTransmission();
+
+    if(error == 0)
+    {
+      snprintf(data, 250, "0x%X", i);
+      Particle.publish("I2C", data);
+      delay(1000);
+    }
+  }
+
   static HTU20DSensor S1tmp(0);
   S1 = &S1tmp;
 
-  static NULLSensor S2tmp(0);
+  static VCNL4010Sensor S2tmp(0);
   S2 = &S2tmp;
 
-  static NULLSensor S3tmp(0);
+  static ADC121CSensor S3tmp(0x50);
   S3 = &S3tmp;
 
-  static NULLSensor S4tmp(0);
+  static ADC121CSensor S4tmp(0x51);
   S4 = &S4tmp;
 
   pinMode(D7, OUTPUT);
@@ -61,5 +77,5 @@ void loop()
     analogWrite(D7, i++);
   }
 
-  delay(60000);
+  delay(delaytime);
 }
