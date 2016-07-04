29a9deb052a085cf1523d12416ff7229c8cae89f
Kabelaffe <kabel42@gmail.com>
2016-07-04 05:42:31 -0700

    Autodetect Sensors

--- CE-Photon.ino
+++ CE-Photon.ino
@@ -1,91 +1,58 @@
 #include <math.h>
-#include <list>
+#include <algorithm>    // std::for_each
+#include <vector>       // std::vector
 
 #include "Sensor.h"
 #include "HTU20D.h"
 #include "ADC121C.h"
 
-Sensor *S1; //I2C 1
-Sensor *S2; //I2C 2
-Sensor *S3; //I2C 3
-Sensor *S4; //I2C 4
+std::vector<Sensor*> sensorList;
 
 char data[255];
 int delaytime = 60000;
 
+int readIfInit(Sensor *S);
+
+int readIfInit(Sensor *S)
+{
+  if(S)
+  {
+    if(S->initOK)
+    {
+      return S->read(data);
+    }
+  }
+  delay(1000);
+  return 0;
+}
+
 void setup()
 {
   //Setup I2C
-  Wire.setSpeed(CLOCK_SPEED_100KHZ);
+  Wire.setSpeed(10000);
   Wire.begin();
 
-  //FIX AM2315
-  Wire.beginTransmission(AM2315_I2CADDR);
-  Wire.endTransmission();
-  delay(500);
-  //End FIX
-
-  for(uint8_t i=1; i<128; i++)
-  {
-    Wire.beginTransmission(i);
-    delay(10);
-    int error = Wire.endTransmission();
-    delay(10);
-
-    if(error == 0)
-    {
-      snprintf(data, 250, "0x%X", i);
-      Particle.publish("I2C", data);
-      delay(1000);
-    }
-  }
-
-  Particle.publish("I2C", "Done");
-
-  static AM2315Sensor S1tmp(0);
-  S1 = &S1tmp;
-
-  static VCNL4010Sensor S2tmp(0);
-  S2 = &S2tmp;
-
-  static MQ131Sensor S3tmp(0);
-  S3 = &S3tmp;
-
-  static MQ4Sensor S4tmp(0);
-  S4 = &S4tmp;
+  //Init Sensors
+  sensorList.push_back(new AM2315Sensor(0));
+  sensorList.push_back(new HTU20DSensor(0));
+  sensorList.push_back(new VCNL4010Sensor(0));
+  sensorList.push_back(new ISL29125Sensor(0));
+  sensorList.push_back(new MQ131Sensor(0x50));
+  sensorList.push_back(new MQ4Sensor(0x51));
+  sensorList.push_back(new MQ135Sensor(0x52));
 
   pinMode(D7, OUTPUT);
 }
 
 void loop()
 {
-  if(S1->initOK)
+  for(Sensor *S: sensorList)
   {
-    int error = S1->read(data);
+    readIfInit(S);
   }
 
-  //S2
-  if(S2->initOK)
-  {
-    int error = S2->read(data);
-  }
-
-  //S3
-  if(S3->initOK)
-  {
-    int error = S3->read(data);
-  }
-
-  //S4
-  if(S4->initOK)
-  {
-    int error = S4->read(data);
-  }
-
-  {
-    static uint8_t i = 0;
-    analogWrite(D7, i++);
-  }
+  static uint8_t i = 0;
+  analogWrite(D7, i++);
 
   delay(delaytime);
 }
