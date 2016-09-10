fdf23b3f40e6742edbc2b5326ae00c05ab94f7a7
Jannis Konrad <kabel42@gmail.com>
2016-09-10 17:37:02 +0200

    Implement Relay Class for Reporter:Box V1

---
+++ Relay/Relay.cpp
@@ -0,0 +1,46 @@
+#include "Relay.h"
+
+Relay::Relay() {
+  //Check addr
+  Wire.beginTransmission(RELAY_I2C_ADDR);
+  int error = Wire.endTransmission();
+
+  if(error == 0)
+  {
+    i2c = true;
+  } else {
+    i2c = false;
+    pinMode(D3, OUTPUT);
+    pinMode(D4, OUTPUT);
+    pinMode(D5, OUTPUT);
+    pinMode(D6, OUTPUT);
+  }
+
+  Particle.function("activate",   &Relay::activateRelay,   this);
+  Particle.function("deactivate", &Relay::deactivateRelay, this);
+}
+
+int Relay::setRelay(long num, bool state)
+{
+  if (num > 0 && num < 5) {
+    if(i2c)
+    {
+      //TODO
+      return -1;
+    } else {
+      digitalWrite(relayPins[num-1], state);
+    }
+    return num;
+  }
+  return 0;
+}
+
+int Relay::activateRelay(String data)
+{
+    return setRelay(data.toInt(), true);
+}
+
+int Relay::deactivateRelay(String data)
+{
+    return setRelay(data.toInt(), true);
+}
---
+++ Relay/Relay.h
@@ -0,0 +1,16 @@
+#ifndef RELAY_H
+#define RELAY_H
+#define RELAY_I2C_ADDR 0x20
+
+#include "application.h"
+
+class Relay {
+  const int relayPins[4] = {D3, D4, D5, D6};
+public:
+  bool i2c;
+  Relay();
+  int setRelay(long num, bool state);
+  int activateRelay(String data);
+  int deactivateRelay(String data);
+};
+#endif
