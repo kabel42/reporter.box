133e8d3d46343ff423452cf66eec0c7af0bdd724
Kabelaffe <kabel42@gmail.com>
2016-09-20 12:49:27 -0700

    refactor getting data from I2C

--- tinyAudio/tinyAudio.cpp
+++ tinyAudio/tinyAudio.cpp
@@ -21,22 +21,44 @@ AudioSensor::AudioSensor(int addr, int delaytime=1000)
   }
 }
 
+float readEnv()
+{
+  Wire.beginTransmission(_addr);
+  Wire.write(0x01);
+  Wire.endTransmission();
+
+  Wire.requestFrom(_addr, 2);
+  if(Wire.available() > 1)
+  {
+    data = Wire.read()<<8;
+    data += Wire.read();
+  }
+
+  return data;
+}
+
+float readGate()
+{
+  Wire.beginTransmission(_addr);
+
+  Wire.write(0x02);
+  Wire.endTransmission();
+
+  Wire.requestFrom(_addr, 2);
+  if(Wire.available() > 1)
+  {
+    data = Wire.read()<<8;
+    data += Wire.read();
+  }
+
+  return data;
+}
+
 float AudioSensor::read()
 {
   if(initOK)
   {
-    Wire.beginTransmission(_addr);
-    Wire.write(0x01);
-    Wire.endTransmission();
-
-    Wire.requestFrom(_addr, 2);
-    if(Wire.available() > 1)
-    {
-      data = Wire.read()<<8;
-      data += Wire.read();
-    }
-
-    return data;
+    return readEnv();
   }
   return nanf("NA");
 }
@@ -48,31 +70,8 @@ int AudioSensor::read(char* status)
     char* ptr = status;
     int size = 0;
 
-    Wire.beginTransmission(_addr);
-
-    Wire.write(0x01);
-    Wire.endTransmission();
-
-    Wire.requestFrom(_addr, 2);
-    if(Wire.available() > 1)
-    {
-      data = Wire.read()<<8;
-      data += Wire.read();
-      publishData(_addr, "ENV", (float)data, (float)data, "ENVALOPE");
-    }
-
-    Wire.beginTransmission(_addr);
-
-    Wire.write(0x02);
-    Wire.endTransmission();
-
-    Wire.requestFrom(_addr, 2);
-    if(Wire.available() > 1)
-    {
-      data = Wire.read()<<8;
-      data += Wire.read();
-      publishData(_addr, "GAT", (float)data, (float)data, "GATE");
-    }
+    publishData(_addr, "ENV", (float)readEnv(), (float)data, "ENVALOPE");
+    publishData(_addr, "GAT", (float)readGate(), (float)data, "GATE");
 
     return 0;
   }
