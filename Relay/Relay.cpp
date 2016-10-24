7199a779743575ba6c7eae930a673889e257c000
Kabelaffe <kabel42@gmail.com>
2016-10-24 08:51:49 -0700

    add support for CE Relay Shield with MCP23008 driver

--- Relay/Relay.cpp
+++ Relay/Relay.cpp
@@ -1,15 +1,28 @@
 #include "Relay.h"
 
-Relay::Relay() {
+Relay::Relay(bool a0, bool a1, bool a2) {
   //Check addr
-  Wire.beginTransmission(RELAY_I2C_ADDR);
+  relay_addr = RELAY_I2C_ADDR | (a0 ? 1 : 0) | (a1 ? 2 : 0) | (a2 ? 4 : 0);
+  Wire.beginTransmission(relay_addr);
   int error = Wire.endTransmission();
 
   if(error == 0)
   {
     i2c = true;
+    initOK = true;
+
+    Wire.beginTransmission(relay_addr);
+    Wire.write(0x00); //IO Direction
+    Wire.write(0xF0); //Set relay as output (IO3-IO0)
+    int error = Wire.endTransmission();
+    if(error)
+    {
+      initOK = false;
+      Particle.publish("error", "relay i2c init failed");
+    }
   } else {
     i2c = false;
+    initOK = true;
     pinMode(D3, OUTPUT);
     pinMode(D4, OUTPUT);
     pinMode(D5, OUTPUT);
@@ -18,6 +31,7 @@ Relay::Relay() {
 
   Particle.function("activate",   &Relay::activateRelay,   this);
   Particle.function("deactivate", &Relay::deactivateRelay, this);
+  Particle.function("getStatus", &Relay::getStatus, this);
 }
 
 int Relay::setRelay(long num, bool newState)
@@ -25,8 +39,21 @@ int Relay::setRelay(long num, bool newState)
   if (num > 0 && num < 5) {
     if(i2c)
     {
-      //TODO
-      return -1;
+      static uint8_t relayStatus = 0;
+      if(newState)
+      {
+        relayStatus |= 1<<(num-1);
+      } else {
+        relayStatus &= ~(1<<(num-1));
+      }
+
+      Wire.beginTransmission(relay_addr);
+      Wire.write(0x09); //GPIO PORT
+      Wire.write(relayStatus); //Set relay status (IO3-IO0)
+      int error = Wire.endTransmission();
+
+      //TODO handle error?
+
     } else {
       digitalWrite(relayPins[num-1], newState);
     }
@@ -45,13 +72,47 @@ int Relay::deactivateRelay(String data)
     return setRelay(data.toInt(), false);
 }
 
+int Relay::getStatus(String data)
+{
+  if(i2c)
+  {
+    Wire.beginTransmission(relay_addr);
+    Wire.write(0x09); //GPIO PORT
+    Wire.endTransmission();
+
+    Wire.requestFrom(relay_addr, (uint8_t)1);
+    if(Wire.available())
+    {
+      status = Wire.read() & 0x0F;
+    } else {
+      return 0;
+    }
+  } else {
+    status = 0;
+    for(uint8_t i = 0; i<4; i++)
+    {
+      status |= digitalRead(relayPins[i])<<i;
+    }
+  }
+  return (int)status;
+}
+
 bool Relay::getRelay(int num)
 {
   if (num > 0 && num < 5) {
     if(i2c)
     {
-      //TODO
-      return false; //TODO
+      Wire.beginTransmission(relay_addr);
+      Wire.write(0x09); //GPIO PORT
+      Wire.endTransmission();
+
+      Wire.requestFrom(relay_addr, (uint8_t)1);
+      if(Wire.available())
+      {
+        status = Wire.read();
+        return ((status>>(num-1))&1);
+      }
+      return false;
     } else {
       digitalRead(relayPins[num-1]);
     }
--- Relay/Relay.h
+++ Relay/Relay.h
@@ -6,12 +6,16 @@
 
 class Relay {
   const int relayPins[4] = {D3, D4, D5, D6};
+  uint8_t relay_addr;
+  uint8_t status;
 public:
   bool i2c;
-  Relay();
+  bool initOK;
+  Relay(bool a0=false, bool a1=false, bool a2=false);
   int setRelay(long num, bool state);
   int activateRelay(String data);
   int deactivateRelay(String data);
+  int getStatus(String data);
   bool getRelay(int num);
 };
 #endif
